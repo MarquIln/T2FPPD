010b9f5b17ea91ca3cf59e63fa1da257e3321499
Lucas Vieira <lpvieira03@gmail.com>
2024-06-02 21:30:21 -0300

    h2o

--- H2O/h2o.go
+++ H2O/h2o.go
@@ -1,18 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 var mutex = NewSemaphore(1)
 var oxygen = 0
 var hydrogen = 0
-var barrier = NewSemaphore(3)
+var barrier = NewBarrier(3)
 var oxyQueue = NewSemaphore(0)
 var hydroQueue = NewSemaphore(0)
 
+// Estrutura do Semáforo
 type Semaphore struct {
 	v    int
 	fila chan struct{}
 	sc   chan struct{}
 }
 
+// NewSemaphore inicializa um semáforo
 func NewSemaphore(init int) *Semaphore {
 	s := &Semaphore{
 		v:    init,
@@ -22,6 +30,7 @@ func NewSemaphore(init int) *Semaphore {
 	return s
 }
 
+// Wait diminui o valor do semáforo e bloqueia se necessário
 func (s *Semaphore) Wait() {
 	s.sc <- struct{}{}
 	s.v--
@@ -33,6 +42,7 @@ func (s *Semaphore) Wait() {
 	}
 }
 
+// Signal aumenta o valor do semáforo e libera uma thread em espera, se necessário
 func (s *Semaphore) Signal() {
 	s.sc <- struct{}{}
 	s.v++
@@ -40,4 +50,100 @@ func (s *Semaphore) Signal() {
 		<-s.fila
 	}
 	<-s.sc
+}
+
+// Estrutura da Barreira
+type Barrier struct {
+	n        int
+	count    int
+	waitCond *sync.Cond
+}
+
+// NewBarrier inicializa uma barreira
+func NewBarrier(n int) *Barrier {
+	return &Barrier{
+		n:        n,
+		waitCond: sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+// Wait bloqueia até que a barreira seja atingida por todas as threads
+func (b *Barrier) Wait() {
+	b.waitCond.L.Lock()
+	b.count++
+	if b.count == b.n {
+		b.count = 0
+		b.waitCond.Broadcast()
+	} else {
+		b.waitCond.Wait()
+	}
+	b.waitCond.L.Unlock()
+}
+
+// Bond simula o processo de ligação para formar uma molécula de água
+func Bond() {
+	fmt.Println("Bonding...")
+	time.Sleep(100 * time.Millisecond) // simula o tempo de ligação
+}
+
+// Função da thread Oxigênio
+func Oxygen() {
+	mutex.Wait()
+	oxygen++
+	if hydrogen >= 2 {
+		hydroQueue.Signal()
+		hydroQueue.Signal()
+		hydrogen -= 2
+		oxyQueue.Signal()
+		oxygen--
+	} else {
+		mutex.Signal()
+	}
+	oxyQueue.Wait()
+	Bond()
+	barrier.Wait()
+	mutex.Signal()
+}
+
+// Função da thread Hidrogênio
+func Hydrogen() {
+	mutex.Wait()
+	hydrogen++
+	if hydrogen >= 2 && oxygen >= 1 {
+		hydroQueue.Signal()
+		hydroQueue.Signal()
+		hydrogen -= 2
+		oxyQueue.Signal()
+		oxygen--
+	} else {
+		mutex.Signal()
+	}
+	hydroQueue.Wait()
+	Bond()
+	barrier.Wait()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	numOxygen := 10
+	numHydrogen := 20
+
+	for i := 0; i < numOxygen; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			Oxygen()
+		}()
+	}
+
+	for i := 0; i < numHydrogen; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			Hydrogen()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Todas as moléculas de água foram formadas.")
 }
